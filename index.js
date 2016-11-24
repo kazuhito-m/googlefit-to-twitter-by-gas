2062cf2982b7e46eecc17e4e33a30f936d9085b1
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-11-25 02:55:19 +0900

    GoogleFitに取得成功した例を追加。

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function getDate() {
-    return 'これが書けたら、まあサンプルは出来上がり。part 2';
+const logger = console;
+
+const scope = ["https://www.googleapis.com/auth/fitness.activity.read"];
+
+let access_token = "";
+
+function getToken(){
+    gapi.auth.authorize({client_id: CLIENT_ID, scope: scope, immediate: false, response_type: "token"}, callback);
+}
+ 
+function callback(e){
+    access_token = e.access_token; 
+    $.ajax({
+        type:"GET",
+        url: "https://www.googleapis.com/fitness/v1/users/me/dataSources",
+        data: {
+            "access_token": access_token
+        },
+        success: function(data){
+            logger.log(data)
+        }
+    });
 }
